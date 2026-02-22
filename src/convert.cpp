a2c4eda5ebac6c4ffa7c23723817945a7e89adef
TLK106 <milenatom25@gmail.com>
2026-02-22 15:00:37 -0500

    Conversion Functions

---
+++ src/convert.cpp
@@ -0,0 +1,20 @@
+int wattsToSolarLuminosity(int watts) {
+    return watts/3.827e26;
+}
+int solarLuminosityToWatts(int solarLuminosity) {
+    return solarLuminosity*3.827e26;
+}
+
+int metersToLightYears(int meters) {
+    return meters/9.461e15;
+}
+int lightYearsToMeters(int lightYears) {
+    return lightYears*9.461e15;
+}
+
+int metersToSolarRadius(int meters) {
+    return meters/6.957e8;
+}
+int solarRadiusToMeters(int solarRadius) {
+    return solarRadius*6.957e8;
+}
---
+++ src/convert.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+int wattsToSolarLuminosity(int watts);
+int solarLuminosityToWatts(int solarLuminosity);
+
+int metersToLightYears(int meters);
+int lightYearsToMeters(int lightYears);
+
+int metersToSolarRadius(int meters);
+int solarRadiusToMeters(int solarRadius);
