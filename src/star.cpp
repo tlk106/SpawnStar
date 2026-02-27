33baa0ba96ef4c48833236ac861576535187a188
TLK106 <milenatom25@gmail.com>
2026-02-26 20:41:34 -0500

    Get the subclass colour as well

--- src/star.cpp
+++ src/star.cpp
@@ -145,30 +145,81 @@ std::string Star::getFinalClass() {
 
 // Rendering
 std::array<int,3> Star::getRenderingColour() {
-  if (getSpectralType()[0] == 'M') {
-    return {255, 170, 70};
-  }
-  else if (getSpectralType()[0] == 'K') {
-    return {255, 174, 107};
-  }
-  else if (getSpectralType()[0] == 'G') {
-    return {255, 235, 209};
-  }
-  else if (getSpectralType()[0] == 'F') {
-    return {255, 244, 245};
-  }
-  else if (getSpectralType()[0] == 'A') {
-    return {224, 230, 255};
-  }
-  else if (getSpectralType()[0] == 'B') {
-    return {186, 204, 255};
-  }
-  else if (getSpectralType()[0] == 'B') {
-    return {186, 204, 255};
-  }
-  else {
-    return {218, 112, 214};
-  }
+  const std::string starClass = getSpectralType();
+
+  // M
+  if (starClass == "M0") {return {255, 195, 139};}
+  if (starClass == "M1") {return {255, 204, 142};}
+  if (starClass == "M2") {return {255, 196, 131};}
+  if (starClass == "M3") {return {255, 206, 129};}
+  if (starClass == "M4") {return {255, 201, 127};}
+  if (starClass == "M5") {return {255, 204, 111};}
+  if (starClass == "M6") {return {255, 195, 112};}
+  if (starClass == "M7") {return {255, 197, 111};}
+  if (starClass == "M8") {return {255, 198, 109};}
+  if (starClass == "M9") {return {255, 198, 108};}
+
+  // K
+  if (starClass == "K0") {return {255, 238, 221};}
+  if (starClass == "K1") {return {255, 224, 188};}
+  if (starClass == "K2") {return {255, 227, 196};}
+  if (starClass == "K3") {return {255, 222, 195};}
+  if (starClass == "K4") {return {255, 216, 181};}
+  if (starClass == "K5") {return {255, 210, 161};}
+  if (starClass == "K6") {return {255, 205, 152};}
+  if (starClass == "K7") {return {255, 199, 142};}
+  if (starClass == "K8") {return {255, 209, 174};}
+  if (starClass == "K9") {return {255, 202, 157};}
+
+  // G
+  if (starClass == "G0") {return {255, 248, 252};}
+  if (starClass == "G1") {return {255, 247, 248};}
+  if (starClass == "G2") {return {255, 245, 242};}
+  if (starClass == "G3") {return {255, 243, 236};}
+  if (starClass == "G4") {return {255, 241, 229};}
+  if (starClass == "G5") {return {255, 244, 234};}
+  if (starClass == "G6") {return {255, 244, 235};}
+  if (starClass == "G7") {return {255, 244, 235};}
+  if (starClass == "G8") {return {255, 237, 222};}
+  if (starClass == "G9") {return {255, 239, 221};}
+
+  // F
+  if (starClass == "F0") {return {224, 229, 255};}
+  if (starClass == "F1") {return {230, 234, 255};}
+  if (starClass == "F2") {return {236, 239, 255};}
+  if (starClass == "F3") {return {230, 233, 255};}
+  if (starClass == "F4") {return {224, 226, 255};}
+  if (starClass == "F5") {return {248, 247, 255};}
+  if (starClass == "F6") {return {244, 241, 255};}
+  if (starClass == "F7") {return {246, 243, 255};}
+  if (starClass == "F8") {return {255, 247, 252};}
+  if (starClass == "F9") {return {255, 247, 252};}
+
+  // A
+  if (starClass == "A0") {return {185, 201, 255};}
+  if (starClass == "A1") {return {181, 199, 255};}
+  if (starClass == "A2") {return {187, 203, 255};}
+  if (starClass == "A3") {return {191, 207, 255};}
+  if (starClass == "A4") {return {197, 211, 255};}
+  if (starClass == "A5") {return {202, 215, 255};}
+  if (starClass == "A6") {return {199, 212, 255};}
+  if (starClass == "A7") {return {200, 213, 255};}
+  if (starClass == "A8") {return {213, 222, 255};}
+  if (starClass == "A9") {return {219, 224, 255};}
+
+  // B
+  if (starClass == "B0") {return {156, 178, 255};}
+  if (starClass == "B1") {return {160, 182, 255};}
+  if (starClass == "B2") {return {160, 180, 255};}
+  if (starClass == "B3") {return {165, 185, 255};}
+  if (starClass == "B4") {return {164, 184, 255};}
+  if (starClass == "B5") {return {170, 191, 255};}
+  if (starClass == "B6") {return {172, 189, 255};}
+  if (starClass == "B7") {return {173, 191, 255};}
+  if (starClass == "B8") {return {177, 195, 255};}
+  if (starClass == "B9") {return {181, 198, 255};}
+
+  return {127, 0, 255};
 }
 
 int Star::getRenderingBrightness() {
