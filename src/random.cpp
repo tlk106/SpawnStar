641f966e259201ced65d54baab0f2527a31fe8e6
TLK106 <milenatom25@gmail.com>
2026-03-01 19:39:32 -0500

    get a random boolean function

--- src/random.cpp
+++ src/random.cpp
@@ -2,17 +2,25 @@
 #include "random.hpp"
 
 int randInt(int min, int max) {
-    std::random_device dev;
-    std::mt19937 rng(dev());
-    std::uniform_int_distribution<std::mt19937::result_type> dist6(min, max);
+  std::random_device dev;
+  std::mt19937 rng(dev());
+  std::uniform_int_distribution<std::mt19937::result_type> dist6(min, max);
 
-    return dist6(rng);
+  return dist6(rng);
 }
 
 float randFloat(float min, float max) {
-    std::random_device dev;
-    std::mt19937 rng(dev());
-    std::uniform_real_distribution<float> dist6(min, max);
+  std::random_device dev;
+  std::mt19937 rng(dev());
+  std::uniform_real_distribution<float> dist6(min, max);
 
-    return dist6(rng);
+  return dist6(rng);
+}
+
+bool randBool() {
+  std::random_device dev;
+  std::mt19937 rng(dev());
+  std::uniform_int_distribution<std::mt19937::result_type> dist6(0, 1);
+
+  return dist6(rng) == 1;
 }
